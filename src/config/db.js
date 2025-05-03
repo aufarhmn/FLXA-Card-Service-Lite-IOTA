df0bc4e0d5530d866a678bf4e8324468c559ba1b
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2025-05-03 13:20:37 +0700

    fix: using conn string

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,7 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  database: process.env.POSTGRES_DB
+  connectionString: process.env.POSTGRE_CONN_STRING,
 })
 
 const checkConnection = async () => {
